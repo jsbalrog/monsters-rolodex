334921fae5b8db331908fd1244384ead12847e93
Ted Jenkins <ted.jenkins@familysearch.org>
2020-11-09 16:30:01 -0700

    Updates

---
+++ precache-manifest.f6b8eda544ee7ce275a9bfddc075ec59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1c8714e40b30fc7c9990225dcd394aa",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2ce394401af42397d76a",
+    "url": "/monsters-rolodex/static/css/main.fbf4cc61.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ce394401af42397d76a",
+    "url": "/monsters-rolodex/static/js/main.fb2ed8bf.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
